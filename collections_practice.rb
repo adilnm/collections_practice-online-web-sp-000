3a4e5b3ed1d954448e8528531e3f1a43c99f62b8
ADIL NAIMI <adilnm23@hotmail.com>
2019-02-23 10:41:29 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -57,6 +57,15 @@ def sum_array(array)
 array.inject{|sum,n| sum+n}
 end
 
+def add_s(array)
+  array.each_with_index.collect do|element, index|
+  if(index!=1)
+  array.push("#{element}s")
+else
+  array.push(element)
+end
+end
+end
 
 
 
